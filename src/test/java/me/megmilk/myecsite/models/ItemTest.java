b617c5d3e5613a44e58358b8cad0633797a30f39
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-03 20:49:15 +0900

    1. ユニットテストテスト名調整。 2. Item オブジェクトから Category オブジェクトが参照できること。

--- src/test/java/me/megmilk/myecsite/models/ItemTest.java
+++ src/test/java/me/megmilk/myecsite/models/ItemTest.java
@@ -11,7 +11,7 @@ public class ItemTest {
      * プライマリキーを指定した検索ができること
      */
     @Test
-    void findTest3() throws SQLException {
+    void findTest1() throws SQLException {
         final int TEST_ID = 1;
         final Item item = Item.find(TEST_ID);
 
@@ -23,10 +23,23 @@ public class ItemTest {
      * 存在しないプライマリキーを指定した場合 null が返ってくること
      */
     @Test
-    void findTest4() throws SQLException {
+    void findTest2() throws SQLException {
         final int INVALID_ID = Integer.MAX_VALUE;
         final Item item = Item.find(INVALID_ID);
 
         assertNull(item);
+    }
+
+    /**
+     * Category オブジェクトも参照できること
+     */
+    @Test
+    void findTest3() throws SQLException {
+        final int TEST_ID = 1;
+        final Item item = Item.find(TEST_ID);
+
+        assert item != null;
+        assertNotNull(item.getCategory());
+        assertEquals(item.getCategory_id(), item.getCategory().getId());
     }
 }
