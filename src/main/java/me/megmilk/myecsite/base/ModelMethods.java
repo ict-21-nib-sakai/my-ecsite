9599f13e06570d4d44b7ec7b74c97966114a4364
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-03 15:33:37 +0900

    ModelAbstract から ModelMethods に、いくつかメソッドを移動した。

--- src/main/java/me/megmilk/myecsite/base/ModelMethods.java
+++ src/main/java/me/megmilk/myecsite/base/ModelMethods.java
@@ -1,5 +1,8 @@
 package me.megmilk.myecsite.base;
 
+import me.megmilk.myecsite.models.User;
+
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -51,5 +54,78 @@ abstract public class ModelMethods extends ModelAbstract {
         }
 
         return properties;
+    }
+
+    /**
+     * @return SELECT で全カラムを取得する文字列
+     */
+    protected static String buildAllColumns(String tableName, HashMap<String, HashMap<String, String>> columns) {
+        final StringBuilder sql = new StringBuilder();
+
+        for (final String columnName : columns.keySet()) {
+            if (sql.length() >= 1) {
+                sql.append(", ");
+            }
+
+            sql.append(
+                String.format(
+                    "%s.%s as %s",
+                    tableName,
+                    columnName,
+                    columns.get(columnName).get("alias")
+                )
+            );
+        }
+
+        return sql.toString();
+    }
+
+    /**
+     * カラム名とその型を返す
+     */
+    protected static HashMap<String, HashMap<String, String>> enumColumns(String tableName) throws SQLException {
+        final String sql =
+            "SELECT"
+                + "   column_name as real_name"
+                + " , '" + tableName + "_' || column_name as alias"
+                + " , data_type"
+                + " FROM information_schema.columns"
+                + " WHERE table_name = ?";
+
+        try (final PreparedStatement statement = prepareStatement(sql)) {
+            statement.setString(1, tableName);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                final HashMap<String, HashMap<String, String>> columns = new HashMap<>();
+                while (resultSet.next()) {
+                    final HashMap<String, String> property = new HashMap<>();
+                    property.put("alias", resultSet.getString("alias"));
+                    property.put("data_type", resultSet.getString("data_type"));
+
+                    columns.put(
+                        resultSet.getString("real_name"),
+                        property
+                    );
+                }
+
+                return columns;
+            }
+        }
+    }
+
+    /**
+     * 論理削除のカラムがある場合、論理削除されたレコードを除外する WHERE 文を付ける
+     */
+    protected static String buildSelectSql(String tableName, HashMap<String, HashMap<String, String>> columns) {
+        final String sql =
+            "SELECT " + User.buildAllColumns(tableName, columns)
+                + " FROM " + tableName
+                + " WHERE 1 = 1 ";
+
+        if (columns.get("deleted_at").isEmpty()) {
+            return sql;
+        }
+
+        return sql + " AND deleted_at is null";
     }
 }
