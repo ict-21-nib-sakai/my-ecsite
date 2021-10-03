91497a2bce02042ff42e5a28bd244dac4abe26ec
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-03 13:52:58 +0900

    categories テーブルに対応するモデルを書いた。

---
+++ src/main/java/me/megmilk/myecsite/models/Category.java
@@ -0,0 +1,122 @@
+package me.megmilk.myecsite.models;
+
+import me.megmilk.myecsite.base.Model;
+
+import java.sql.*;
+import java.util.HashMap;
+
+public class Category extends Model {
+    /**
+     * テーブル名
+     */
+    private final static String TABLE_NAME = "categories";
+
+    /**
+     * カラム名, エイリアス, 型名
+     */
+    private static HashMap<String, HashMap<String, String>> columns = new HashMap<>();
+
+    /**
+     * カラム名とその値
+     */
+    private final HashMap<String, Object> properties = new HashMap<>();
+
+    /**
+     * カラム名、そのエイリアス、その型を代入する
+     */
+    public static void setupColumns() throws SQLException {
+        if (columns.size() >= 1) {
+            return;
+        }
+
+        columns = enumColumns(TABLE_NAME);
+    }
+
+    /**
+     * プライマリキーによる検索
+     */
+    public static Category find(int id) throws SQLException {
+        setupColumns();
+
+        final String sql = buildSelectSql(TABLE_NAME, columns)
+            + " AND id = ?";
+
+        try (final PreparedStatement statement = Model.prepareStatement(sql)) {
+            statement.setInt(1, id);
+
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                if (!resultSet.next()) {
+                    return null;
+                }
+
+                return make(resultSet);
+            }
+        }
+    }
+
+    /**
+     * @return ResultSet から Category オブジェクトにする
+     */
+    public static Category make(ResultSet resultSet) throws SQLException {
+        Category category = new Category();
+
+        for (String columnName : columns.keySet()) {
+            switch (columns.get(columnName).get("data_type")) {
+                case "integer":
+                    category.properties.put(
+                        columnName,
+                        resultSet.getInt(columns.get(columnName).get("alias"))
+                    );
+                    break;
+
+                case "character varying":
+                case "text":
+                    category.properties.put(
+                        columnName,
+                        resultSet.getString(columns.get(columnName).get("alias"))
+                    );
+                    break;
+
+                case "boolean":
+                    category.properties.put(
+                        columnName,
+                        resultSet.getBoolean(columns.get(columnName).get("alias"))
+                    );
+                    break;
+
+                case "timestamp without time zone":
+                    category.properties.put(
+                        columnName,
+                        resultSet.getTimestamp(columns.get(columnName).get("alias"))
+                    );
+                    break;
+            }
+        }
+
+        return category;
+    }
+
+    public int getId() {
+        return (int) properties.get("id");
+    }
+
+    public String getName() {
+        return (String) properties.get("name");
+    }
+
+    public boolean isSuspended() {
+        return (boolean) properties.get("suspended");
+    }
+
+    public Timestamp getCreated_at() {
+        return (Timestamp) properties.get("created_at");
+    }
+
+    public Timestamp getUpdated_at() {
+        return (Timestamp) properties.get("updated_at");
+    }
+
+    public Timestamp getDeleted_at() {
+        return (Timestamp) properties.get("deleted_at");
+    }
+}
