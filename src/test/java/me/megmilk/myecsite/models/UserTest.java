989217dbe4d2f8387a4fc228143aac265e875eb9
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-03 10:03:17 +0900

    Userモデルの単体テスト

---
+++ src/test/java/me/megmilk/myecsite/models/UserTest.java
@@ -0,0 +1,53 @@
+package me.megmilk.myecsite.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    /**
+     * email を指定した検索ができること
+     */
+    @Test
+    void findTest1() throws SQLException {
+        final String TEST_EMAIL = "taro@example.com";
+        final User user = User.find(TEST_EMAIL);
+
+        assertNotNull(user);
+        assertEquals(TEST_EMAIL, user.getEmail());
+    }
+
+    /**
+     * 存在しない email を指定した場合 null が返ってくること
+     */
+    @Test
+    void findTest2() throws SQLException {
+        final String INVALID_EMAIL = "invalid@example.com";
+        final User user = User.find(INVALID_EMAIL);
+
+        assertNull(user);
+    }
+
+    /**
+     * プライマリキーを指定した検索ができること
+     */
+    @Test
+    void findTest3() throws SQLException {
+        final int TEST_ID = 1;
+        final User user = User.find(TEST_ID);
+
+        assertNotNull(user);
+        assertEquals(TEST_ID, user.getId());
+    }
+
+    /**
+     * 存在しないプライマリキーを指定した場合 null が返ってくること
+     */
+    @Test
+    void findTest4() throws SQLException {
+        final int INVALID_ID = Integer.MAX_VALUE;
+        final User user = User.find(INVALID_ID);
+
+        assertNull(user);
+    }
+}
