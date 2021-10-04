1c160a62b1335294b5a413d7fcb97dda3c855bcd
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-04 09:38:13 +0900

    商品名部分一致による検索の単体テスト

--- src/test/java/me/megmilk/myecsite/models/ItemTest.java
+++ src/test/java/me/megmilk/myecsite/models/ItemTest.java
@@ -3,6 +3,7 @@ package me.megmilk.myecsite.models;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,5 +42,47 @@ public class ItemTest {
         assert item != null;
         assertNotNull(item.getCategory());
         assertEquals(item.getCategory_id(), item.getCategory().getId());
+    }
+
+    /**
+     * 商品名の部分一致による商品検索
+     */
+    @Test
+    void searchTest1() throws SQLException {
+        final String TEST_ITEM_NAME = "猫";
+        final int TEST_LIMIT = 3;
+        final int TEST_OFFSET = 0;
+
+        final List<Item> items = Item.search(
+            TEST_ITEM_NAME,
+            TEST_LIMIT,
+            TEST_OFFSET
+        );
+
+        assertTrue(items.size() >= 1);
+        assertTrue(items.size() <= TEST_LIMIT);
+        for (Item item : items) {
+            assertTrue(
+                item.getName().contains(TEST_ITEM_NAME)
+            );
+        }
+    }
+
+    /**
+     * 商品名の部分一致による商品検索 (1件もヒットしない場合)
+     */
+    @Test
+    void searchTest2() throws SQLException {
+        final String TEST_INVALID_ITEM_NAME = "無効な商品名";
+        final int TEST_LIMIT = 3;
+        final int TEST_OFFSET = 0;
+
+        final List<Item> items = Item.search(
+            TEST_INVALID_ITEM_NAME,
+            TEST_LIMIT,
+            TEST_OFFSET
+        );
+
+        assertEquals(0, items.size());
     }
 }
