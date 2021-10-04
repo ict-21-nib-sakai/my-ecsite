dbd3d01a33d94666d9b30deacad23a52944229eb
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-04 21:27:41 +0900

    受け取るエンコーディングは UFT-8 とする。

---
+++ src/main/java/me/megmilk/myecsite/controllers/filters/AcceptEncodingFilter.java
@@ -0,0 +1,24 @@
+package me.megmilk.myecsite.controllers.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "AcceptEncodingFilter", urlPatterns = {"/*"})
+public class AcceptEncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain
+    ) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
