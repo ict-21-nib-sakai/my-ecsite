5a6c61b198279176edfc5d802a6de7ff18d87d7d
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-04 17:50:43 +0900

    注文詳細の単体テスト

---
+++ src/test/java/me/megmilk/myecsite/models/OrderDetailTest.java
@@ -0,0 +1,25 @@
+package me.megmilk.myecsite.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderDetailTest {
+    /**
+     * プライマリキーを指定した注文詳細が検索できること
+     */
+    @Test
+    void findTest1() throws SQLException {
+        final int TEST_ID = 1;
+        final OrderDetail orderDetail = OrderDetail.find(TEST_ID);
+
+        assertNotNull(orderDetail);
+        assertEquals(TEST_ID, orderDetail.getOrder_id());
+        assertNotNull(orderDetail.getOrder());
+
+        // 循環参照を防ぐため order_details プロパティは null であること
+        assertNull(orderDetail.getOrder().getOrderDetails());
+    }
+}
