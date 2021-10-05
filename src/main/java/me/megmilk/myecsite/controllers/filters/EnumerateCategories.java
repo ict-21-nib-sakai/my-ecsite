239fb9b53dded8168a52fc7707f933abfcb56905
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-05 16:43:08 +0900

    カテゴリ一覧の取得は、すべてのページで必要と考えるので、フィルーター側で取得して、リクエストスコープとして、ビュー側に渡した。

---
+++ src/main/java/me/megmilk/myecsite/controllers/filters/EnumerateCategories.java
@@ -0,0 +1,37 @@
+package me.megmilk.myecsite.controllers.filters;
+import me.megmilk.myecsite.models.Category;
+import me.megmilk.myecsite.services.CategoryService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebFilter(filterName = "EnumerateCategories")
+public class EnumerateCategories implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        List<Category> categories = new ArrayList<>();
+
+        try {
+            categories = CategoryService.enumerate();
+        } catch (SQLException e) {
+            // TODO ログに記録, エラーページを表示
+            e.printStackTrace();
+        }
+
+        request.setAttribute("categories", categories);
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
