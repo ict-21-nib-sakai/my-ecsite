7252517fe152e51c221c152d28c102ca443dfd3b
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-08 09:03:51 +0900

    ログインコントローラー訂正。ログイン済みの場合のリダイレクト先が間違っていまいした。

--- src/main/java/me/megmilk/myecsite/controllers/LoginController.java
+++ src/main/java/me/megmilk/myecsite/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package me.megmilk.myecsite.controllers;
 
 import me.megmilk.myecsite.models.User;
+import me.megmilk.myecsite.services.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * 一般側ログイン
@@ -34,7 +36,9 @@ public class LoginController extends HttpServlet {
 
         // ログイン済みの場合、Web サイトのトップページにリダイレクトする
         if (null != user) {
-            response.sendRedirect("/");
+            response.sendRedirect(
+                request.getContextPath()
+            );
 
             return;
         }
