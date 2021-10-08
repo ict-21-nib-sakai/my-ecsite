1c53f695811eaad3fea882be913c7fff62534114
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-08 13:38:11 +0900

    ログアウトコントローラー。 GET リクエストにしようか考えたが。いくつか記事を読んだが GET リクエストでのログアウトは過去のもの。 @see https://www.nao000.com/logout-post-vs-get/

---
+++ src/main/java/me/megmilk/myecsite/controllers/LogoutController.java
@@ -0,0 +1,50 @@
+package me.megmilk.myecsite.controllers;
+
+import me.megmilk.myecsite.services.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 一般側ログアウト
+ */
+@WebServlet(name = "logout", urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
+    public void init() {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    /**
+     * GET でのログアウトは想定外
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response
+    ) throws ServletException, IOException {
+        // Web サイトのトップページに遷移するだけ
+        response.sendRedirect(
+            request.getContextPath()
+        );
+    }
+
+    /**
+     * ログアウト後は Web サイトのトップページに移動
+     */
+    @Override
+    protected void doPost(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws ServletException, IOException {
+        UserService.logout(request);
+
+        response.sendRedirect(
+            request.getContextPath()
+        );
+    }
+}
