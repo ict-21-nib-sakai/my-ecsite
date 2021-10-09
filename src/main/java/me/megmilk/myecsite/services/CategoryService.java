658f470d1a865af518fdb0cd4c55577f4c0d9b2a
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-09 09:01:27 +0900

    カテゴリサービス。プライマリキーによる カテゴリ検索

--- src/main/java/me/megmilk/myecsite/services/CategoryService.java
+++ src/main/java/me/megmilk/myecsite/services/CategoryService.java
@@ -2,6 +2,7 @@ package me.megmilk.myecsite.services;
 
 import me.megmilk.myecsite.models.Category;
 
+import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -11,5 +12,17 @@ public class CategoryService {
      */
     public static List<Category> enumerate() throws SQLException {
         return Category.enumerate();
+    }
+
+    /**
+     * プライマリキーによる検索
+     */
+    public static Category find(HttpServletRequest request
+    ) throws SQLException, NumberFormatException {
+        final int categoryId = Integer.parseInt(
+            request.getPathInfo().substring(1)
+        );
+
+        return Category.find(categoryId);
     }
 }
