4e469a15a7e0c584e3d68769edcf081404b9b678
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-12 17:09:26 +0900

    OrderDetail モデルの単体テスト。 テストを実行前にデータベース内の各テーブル、各レコードを初期化した。

--- src/test/java/me/megmilk/myecsite/models/OrderDetailTest.java
+++ src/test/java/me/megmilk/myecsite/models/OrderDetailTest.java
@@ -2,16 +2,19 @@ package me.megmilk.myecsite.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OrderDetailTest {
+public class OrderDetailTest extends AbstractTest {
     /**
      * プライマリキーを指定した注文詳細が検索できること
      */
     @Test
-    void findTest1() throws SQLException {
+    void findTest1() throws SQLException, IOException {
+        seed();
+
         final int TEST_ID = 1;
         final OrderDetail orderDetail = OrderDetail.find(TEST_ID);
 
