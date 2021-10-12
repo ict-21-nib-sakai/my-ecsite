cdc0c8aaa0baeca438cea87712b75c0590dbca77
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-12 15:33:43 +0900

    [WIP] カートモデルの単体テスト。

--- src/test/java/me/megmilk/myecsite/models/CartTest.java
+++ src/test/java/me/megmilk/myecsite/models/CartTest.java
@@ -3,6 +3,7 @@ package me.megmilk.myecsite.models;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -40,5 +41,31 @@ public class CartTest {
         final Cart cart = Cart.find(TEST_INVALID_CART_ID);
 
         assertNull(cart);
+    }
+
+    /**
+     * ユーザーIDを指定したカート内一覧取得
+     */
+    @Test
+    void enumerateTest1() throws SQLException {
+        final int TEST_USER_ID = 1;
+        final List<Cart> carts = Cart.enumerate(TEST_USER_ID);
+
+        assertTrue(carts.size() >= 1);
+        for (Cart cart : carts) {
+            assertEquals(TEST_USER_ID, cart.getUser_id());
+            assertEquals(TEST_USER_ID, cart.getUser().getId());
+        }
+    }
+
+    /**
+     * 無効なユーザーIDを指定したカート内一覧取得
+     */
+    @Test
+    void enumerateTest2() throws SQLException {
+        final int TEST_INVALID_USER_ID = Integer.MAX_VALUE;
+        final List<Cart> carts = Cart.enumerate(TEST_INVALID_USER_ID);
+
+        assertEquals(0, carts.size());
     }
 }
