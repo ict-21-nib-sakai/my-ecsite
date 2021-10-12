105b350f2344e58bb7dd6ed8e73dc977090dfa6e
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-12 17:11:37 +0900

    User モデルの単体テスト。 各テストを実行前にデータベース内の各テーブル、各レコードを初期化した。

--- src/test/java/me/megmilk/myecsite/models/UserTest.java
+++ src/test/java/me/megmilk/myecsite/models/UserTest.java
@@ -1,15 +1,19 @@
 package me.megmilk.myecsite.models;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 
-public class UserTest {
+public class UserTest extends AbstractTest {
     /**
      * email を指定した検索ができること
      */
     @Test
-    void findTest1() throws SQLException {
+    void findTest1() throws SQLException, IOException {
+        seed();
+
         final String TEST_EMAIL = "taro@example.com";
         final User user = User.find(TEST_EMAIL);
 
@@ -21,7 +25,9 @@ public class UserTest {
      * 存在しない email を指定した場合 null が返ってくること
      */
     @Test
-    void findTest2() throws SQLException {
+    void findTest2() throws SQLException, IOException {
+        seed();
+
         final String INVALID_EMAIL = "invalid@example.com";
         final User user = User.find(INVALID_EMAIL);
 
@@ -32,7 +38,9 @@ public class UserTest {
      * プライマリキーを指定した検索ができること
      */
     @Test
-    void findTest3() throws SQLException {
+    void findTest3() throws SQLException, IOException {
+        seed();
+
         final int TEST_ID = 1;
         final User user = User.find(TEST_ID);
 
@@ -44,7 +52,9 @@ public class UserTest {
      * 存在しないプライマリキーを指定した場合 null が返ってくること
      */
     @Test
-    void findTest4() throws SQLException {
+    void findTest4() throws SQLException, IOException {
+        seed();
+
         final int INVALID_ID = Integer.MAX_VALUE;
         final User user = User.find(INVALID_ID);
 
