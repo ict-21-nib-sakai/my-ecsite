aa5af77dfdc6d6c4d8f35aaaec6cb5f10f3f98e4
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-20 17:58:03 +0900

    [WIP] CartPaymentController 配達先・お支払い方法 のコントローラー書きかけ。

--- src/main/java/me/megmilk/myecsite/http/controllers/CartPaymentController.java
+++ src/main/java/me/megmilk/myecsite/http/controllers/CartPaymentController.java
@@ -1,11 +1,17 @@
 package me.megmilk.myecsite.http.controllers;
 
+import me.megmilk.myecsite.models.Cart;
+import me.megmilk.myecsite.services.CartService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 配達先・お支払い方法入力ページ
@@ -28,10 +34,35 @@ public class CartPaymentController extends HttpServlet {
         // このページはWebブラウザにキャシュさせない
         response.addHeader("Cache-Control", "no-cache, no-store");
 
-        // TODO カートに商品が1つもない場合、カートページにリダイレクトする
+        List<Cart> carts = new ArrayList<>();
+        int sum = 0;
+        int totalQuantity = 0;
 
-        // TODO implements
-        System.out.println("TODO cart_payment");
+        try {
+            carts = CartService.enumerate(request);
+            sum = CartService.sum(carts);
+            totalQuantity = CartService.totalQuantity(request);
+        } catch (SQLException e) {
+            // TODO ログ, エラーページ表示
+            e.printStackTrace();
+        }
+
+        // カートに商品が1つもない場合、カートページにリダイレクトする
+        if (0 == carts.size()) {
+            response.sendRedirect(
+                request.getContextPath() + "/cart"
+            );
+
+            return;
+        }
+
+        request.setAttribute("carts", carts);
+        request.setAttribute("totalQuantity", totalQuantity);
+        request.setAttribute("sum", sum);
+
+        request
+            .getRequestDispatcher("/WEB-INF/views/cart_payment.jsp")
+            .forward(request, response);
     }
 
     /**
