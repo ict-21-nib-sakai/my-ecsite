7285ec96f3e67021066eeeb8e48f6f61f8f99337
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-21 13:00:44 +0900

    CartPaymentController コントローラーから明示的に渡していた定数を取り除いた。

--- src/main/java/me/megmilk/myecsite/http/controllers/CartPaymentController.java
+++ src/main/java/me/megmilk/myecsite/http/controllers/CartPaymentController.java
@@ -59,8 +59,6 @@ public class CartPaymentController extends HttpServlet {
         request.setAttribute("carts", carts);
         request.setAttribute("totalQuantity", totalQuantity);
         request.setAttribute("sum", sum);
-        request.setAttribute("DELIVERY_OPTIONS", CartService.DELIVERY_OPTIONS);
-        request.setAttribute("DELIVERY_OPTIONAL", CartService.DELIVERY_OPTIONAL);
 
         request
             .getRequestDispatcher("/WEB-INF/views/cart_payment.jsp")
