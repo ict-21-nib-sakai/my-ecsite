07e4e65c87a7e2fd11b9fcb18d8b61aee91ebca6
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-10-26 08:39:03 +0900

    CartPaymentValidator.java 配達先・お支払い方法 フォームのバリデーションの粒度を、入力欄単位に分けた。

--- src/main/java/me/megmilk/myecsite/http/validators/CartPaymentValidator.java
+++ src/main/java/me/megmilk/myecsite/http/validators/CartPaymentValidator.java
@@ -17,72 +17,99 @@ public class CartPaymentValidator {
      * </dl>
      */
     public static boolean validate(HttpServletRequest request) {
-        //<editor-fold desc="配達先のラジオボタン">
+        // 配達先のラジオボタン
         final String deliveryOption = request.getParameter("delivery_option");
 
-        {
-            boolean found = false;
+        // 配達先の選択肢をバリデーション
+        validateDeliveryOption(request);
 
-            for (String[] option : CartService.DELIVERY_OPTIONS) {
-                if (null == deliveryOption) {
-                    break;
-                }
+        // ご自宅以外の配達先住所をバリデーション
+        validateOptionalAddress(request);
 
-                if (option[0].equals(deliveryOption)) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                FlashBag.setMessagesBag(request, "delivery_option", "どれか選択してください。");
-            }
-        }
-        //</editor-fold>
-
-        //<editor-fold desc="ご自宅以外の配達先住所">
-        if (deliveryOption != null
-            && deliveryOption.equals(CartService.DELIVERY_OPTIONAL)
-        ) {
-            final String optionalAddress = request.getParameter("optional_address");
-
-            if (null == optionalAddress || 0 == optionalAddress.length()) {
-                FlashBag.setMessagesBag(request, "optional_address", "入力してください。");
-            }
-
-            if (null != optionalAddress && optionalAddress.length() > 255) {
-                FlashBag.setMessagesBag(request, "optional_address", "255文字以内で入力してください。");
-            }
-        }
-        //</editor-fold>
-
-        //<editor-fold desc="お支払い方法">
-        {
-            final String paymentMethod = request.getParameter("payment_method");
-
-            boolean found = false;
-
-            for (String[] option : CartService.PAYMENT_METHODS) {
-                if (null == paymentMethod) {
-                    break;
-                }
-
-                if (option[0].equals(paymentMethod)) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                FlashBag.setMessagesBag(request, "payment_method", "どれか選択してください。");
-            }
-        }
-        //</editor-fold>
+        // お支払い方法"
+        validatePaymentMethod(request);
 
         if (FlashBag.hasAnyMessage(request)) {
             FlashBag.setErrorTitle(request, "入力に不備があります。");
         }
 
         return !FlashBag.hasAnyMessage(request);
+    }
+
+    /**
+     * 配達先の選択肢をバリデーション
+     */
+    private static void validateDeliveryOption(HttpServletRequest request) {
+        final String deliveryOption = request.getParameter("delivery_option");
+
+        boolean found = false;
+
+        for (String[] option : CartService.DELIVERY_OPTIONS) {
+            if (null == deliveryOption) {
+                break;
+            }
+
+            if (option[0].equals(deliveryOption)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            FlashBag.setMessagesBag(request, "delivery_option", "どれか選択してください。");
+        }
+    }
+
+    /**
+     * ご自宅以外の配達先をバリデーション
+     */
+    private static void validateOptionalAddress(HttpServletRequest request) {
+        // 配達先のラジオボタン
+        final String deliveryOption = request.getParameter("delivery_option");
+
+        // ラジオボタンが未選択の場合は、バリデーションできないので無視する
+        if (null == deliveryOption) {
+            return;
+        }
+
+        // ラジオボタンが「配達先を指定」じゃない場合は、対象外なので無視する
+        if (!CartService.DELIVERY_OPTIONAL.equals(deliveryOption)) {
+            return;
+        }
+
+        // ご自宅以外の配達先
+        final String optionalAddress = request.getParameter("optional_address");
+
+        if (null == optionalAddress || 0 == optionalAddress.length()) {
+            FlashBag.setMessagesBag(request, "optional_address", "入力してください。");
+        }
+
+        if (null != optionalAddress && optionalAddress.length() > 255) {
+            FlashBag.setMessagesBag(request, "optional_address", "255文字以内で入力してください。");
+        }
+    }
+
+    /**
+     * お支払い方法をバリデーション
+     */
+    private static void validatePaymentMethod(HttpServletRequest request) {
+        final String paymentMethod = request.getParameter("payment_method");
+
+        boolean found = false;
+
+        for (String[] option : CartService.PAYMENT_METHODS) {
+            if (null == paymentMethod) {
+                break;
+            }
+
+            if (option[0].equals(paymentMethod)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            FlashBag.setMessagesBag(request, "payment_method", "どれか選択してください。");
+        }
     }
 }
