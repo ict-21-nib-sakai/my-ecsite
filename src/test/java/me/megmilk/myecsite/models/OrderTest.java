23f40b39aad2a32dc387a09aa2b7fe7a1d435b6c
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-11-01 09:27:08 +0900

    Order モデルに追加した add メソッドの単体テストを書いた。

--- src/test/java/me/megmilk/myecsite/models/OrderTest.java
+++ src/test/java/me/megmilk/myecsite/models/OrderTest.java
@@ -1,5 +1,6 @@
 package me.megmilk.myecsite.models;
 
+import me.megmilk.myecsite.services.CartService;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -22,5 +23,33 @@ public class OrderTest extends AbstractTest {
         assertNotNull(order.getUser());
         assertEquals(order.getUser_id(), order.getUser().getId());
         assertTrue(order.getOrderDetails().size() >= 1);
+    }
+
+    /**
+     * 注文確定のレコードが追加できること
+     */
+    @Test
+    void addTest1() throws SQLException, IOException {
+        seed();
+
+        final int TEST_USER_ID = 1;
+        final User TEST_USER = User.find(TEST_USER_ID);
+        final String TEST_PAYMENT_METHOD = CartService.PAYMENT_METHOD_CARD;
+        final String TEST_SHIPPING_ADDRESS_TYPE = CartService.DELIVERY_OPTIONAL;
+        final String TEST_SHIPPING_ADDRESS = "鳥取県○○市○○町○○123";
+
+        assert TEST_USER != null;
+        Order order = Order.add(
+            TEST_USER,
+            TEST_PAYMENT_METHOD,
+            TEST_SHIPPING_ADDRESS_TYPE,
+            TEST_SHIPPING_ADDRESS
+        );
+
+        assertNotNull(order);
+        assertEquals(TEST_USER_ID, order.getUser_id());
+        assertEquals(TEST_PAYMENT_METHOD, order.getPayment_method());
+        assertEquals(TEST_SHIPPING_ADDRESS_TYPE, order.getShipping_address_type());
+        assertEquals(TEST_SHIPPING_ADDRESS, order.getShipping_address());
     }
 }
