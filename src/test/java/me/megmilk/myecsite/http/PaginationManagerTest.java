89a8dc1b2d235081417367d618b8c67194a53fc5
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-11-07 17:34:32 +0900

    [WIP] 自作したページネーションの単体テスト

--- src/test/java/me/megmilk/myecsite/http/PaginationManagerTest.java
+++ src/test/java/me/megmilk/myecsite/http/PaginationManagerTest.java
@@ -198,4 +198,124 @@ public class PaginationManagerTest {
         final int EXPECTED_NUMBER = 2;
         assertEquals(EXPECTED_NUMBER, paginator.calcStartPageNumber());
     }
+
+    /**
+     * リンクとして表示する最後のページ番号を計算する
+     */
+    @Test
+    void calcLastPageNumberTest1() {
+        final int TEST_TOTAL_COUNT = 202;
+        final int TEST_ITEMS_PER_PAGE = 20;
+        // 現在閲覧中のページは「1」ページ目と想定したテスト
+        final int TEST_CURRENT_PAGE = 1;
+        final int TEST_DISPLAY_LINKS = 5;
+        final int TEST_MARGIN = 2;
+
+        PaginationManager paginator = new PaginationManager(
+            TEST_TOTAL_COUNT,
+            TEST_ITEMS_PER_PAGE,
+            TEST_CURRENT_PAGE,
+            TEST_DISPLAY_LINKS,
+            TEST_MARGIN
+        );
+
+        final int EXPECTED_NUMBER = 5;
+        assertEquals(EXPECTED_NUMBER, paginator.calcLastPageNumber());
+    }
+
+    /**
+     * リンクとして表示する最後のページ番号を計算する
+     */
+    @Test
+    void calcLastPageNumberTest2() {
+        final int TEST_TOTAL_COUNT = 202;
+        final int TEST_ITEMS_PER_PAGE = 20;
+        // 現在閲覧中のページは「2」ページ目と想定したテスト
+        final int TEST_CURRENT_PAGE = 2;
+        final int TEST_DISPLAY_LINKS = 5;
+        final int TEST_MARGIN = 2;
+
+        PaginationManager paginator = new PaginationManager(
+            TEST_TOTAL_COUNT,
+            TEST_ITEMS_PER_PAGE,
+            TEST_CURRENT_PAGE,
+            TEST_DISPLAY_LINKS,
+            TEST_MARGIN
+        );
+
+        final int EXPECTED_NUMBER = 5;
+        assertEquals(EXPECTED_NUMBER, paginator.calcLastPageNumber());
+    }
+
+    /**
+     * リンクとして表示する最後のページ番号を計算する
+     */
+    @Test
+    void calcLastPageNumberTest3() {
+        final int TEST_TOTAL_COUNT = 202;
+        final int TEST_ITEMS_PER_PAGE = 20;
+        // 現在閲覧中のページは「3」ページ目と想定したテスト
+        final int TEST_CURRENT_PAGE = 3;
+        final int TEST_DISPLAY_LINKS = 5;
+        final int TEST_MARGIN = 2;
+
+        PaginationManager paginator = new PaginationManager(
+            TEST_TOTAL_COUNT,
+            TEST_ITEMS_PER_PAGE,
+            TEST_CURRENT_PAGE,
+            TEST_DISPLAY_LINKS,
+            TEST_MARGIN
+        );
+
+        final int EXPECTED_NUMBER = 5;
+        assertEquals(EXPECTED_NUMBER, paginator.calcLastPageNumber());
+    }
+
+    /**
+     * リンクとして表示する最後のページ番号を計算する
+     */
+    @Test
+    void calcLastPageNumberTest4() {
+        final int TEST_TOTAL_COUNT = 202;
+        final int TEST_ITEMS_PER_PAGE = 20;
+        // 現在閲覧中のページは「4」ページ目と想定したテスト
+        final int TEST_CURRENT_PAGE = 4;
+        final int TEST_DISPLAY_LINKS = 5;
+        final int TEST_MARGIN = 2;
+
+        PaginationManager paginator = new PaginationManager(
+            TEST_TOTAL_COUNT,
+            TEST_ITEMS_PER_PAGE,
+            TEST_CURRENT_PAGE,
+            TEST_DISPLAY_LINKS,
+            TEST_MARGIN
+        );
+
+        final int EXPECTED_NUMBER = 6;
+        assertEquals(EXPECTED_NUMBER, paginator.calcLastPageNumber());
+    }
+
+    /**
+     * リンクとして表示する最後のページ番号を計算する
+     */
+    @Test
+    void calcLastPageNumberTest5() {
+        final int TEST_TOTAL_COUNT = 202;
+        final int TEST_ITEMS_PER_PAGE = 20;
+        // 現在閲覧中のページは「5」ページ目と想定したテスト
+        final int TEST_CURRENT_PAGE = 5;
+        final int TEST_DISPLAY_LINKS = 5;
+        final int TEST_MARGIN = 2;
+
+        PaginationManager paginator = new PaginationManager(
+            TEST_TOTAL_COUNT,
+            TEST_ITEMS_PER_PAGE,
+            TEST_CURRENT_PAGE,
+            TEST_DISPLAY_LINKS,
+            TEST_MARGIN
+        );
+
+        final int EXPECTED_NUMBER = 7;
+        assertEquals(EXPECTED_NUMBER, paginator.calcLastPageNumber());
+    }
 }
