1db4a562d39a81a5f40e4022de1367854bb5633d
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-11-08 14:42:00 +0900

    PaginationManager - GETリクエストのパラメータ名とその値を保持。 - パラメーター付きURLを組み立てる

--- src/main/java/me/megmilk/myecsite/http/PaginationManager.java
+++ src/main/java/me/megmilk/myecsite/http/PaginationManager.java
@@ -1,5 +1,12 @@
 package me.megmilk.myecsite.http;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class PaginationManager {
     final private int totalCount;
     final private int itemsPerPage;
@@ -9,6 +16,15 @@ public class PaginationManager {
     final private int endOfPage;
 
     /**
+     * GETリクエストのパラメータ名とその値
+     * <p>
+     * 順序を保つため List でラッピングしています。
+     */
+    private final List<HashMap<String, String[]>> params = new ArrayList<>();
+
+    /**
+     * コンストラクタ
+     *
      * @param totalCount   すべての件数
      * @param itemsPerPage 1ページあたりの表示件数
      * @param currentPage  現在のページ番号。開始番号は1です。
@@ -39,6 +55,48 @@ public class PaginationManager {
 
     public int getCurrentPage() {
         return currentPage;
+    }
+
+    /**
+     * パラメータ名と値を内部の変数に保持する
+     */
+    public void setParameters(HttpServletRequest request, String[] paramNames) {
+        for (String paramName : paramNames) {
+            final HashMap<String, String[]> param = new HashMap<>();
+            param.put(paramName, request.getParameterValues(paramName));
+
+            params.add(param);
+        }
+    }
+
+    /**
+     * パラメーター付きURLを組み立てる
+     */
+    public String buildUrl(int pageNumber) throws UnsupportedEncodingException {
+        StringBuilder queryParam = new StringBuilder();
+
+        for (final HashMap<String, String[]> param : this.params) {
+            for (final String key : param.keySet()) {
+                final String[] values = param.get(key);
+
+                if (null == values) {
+                    continue;
+                }
+
+                for (String value : values) {
+                    queryParam
+                        .append(key)
+                        .append("=")
+                        .append(URLEncoder.encode(value, "UTF-8"))
+                        .append("&")
+                    ;
+                }
+            }
+        }
+
+        queryParam.append("p=").append(pageNumber);
+
+        return "?" + queryParam;
     }
 
     /**
