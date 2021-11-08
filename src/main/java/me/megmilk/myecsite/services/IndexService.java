0a9330e1b58efc9e719fe7376ca698f57b8a7330
🗻Kiyonori SAKAI🏂 <ict21sakai@outlook.jp>
2021-11-08 10:23:51 +0900

    [WIP] IndexService - ページネーションのインスタンスを組むメソッドを書いた。 - 商品名の部分一致による商品検索結果が、全部で何件か (Item モデルのメソッドのラッピング関数。)
    TODO
    - URL や パラメーターの情報を a タグの href 属性に含めること。今の時点では「#」になっている。

--- src/main/java/me/megmilk/myecsite/services/IndexService.java
+++ src/main/java/me/megmilk/myecsite/services/IndexService.java
@@ -1,5 +1,6 @@
 package me.megmilk.myecsite.services;
 
+import me.megmilk.myecsite.http.PaginationManager;
 import me.megmilk.myecsite.models.Item;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,12 @@ import java.util.List;
 public class IndexService {
     /** 1ページに表示する商品数 */
     static final int ITEMS_PER_PAGE = 20;
+
+    /** ページネーションのリンクとして表示する数 */
+    static final int DISPLAY_LINKS = 5;
+
+    /** 読み去ったページを左側にリンクとして表示する数 */
+    static final int LEFT_MARGIN = 2;
 
     /**
      * 商品名の部分一致による商品検索
@@ -32,5 +39,38 @@ public class IndexService {
             ITEMS_PER_PAGE,
             page * ITEMS_PER_PAGE
         );
+    }
+
+    /**
+     * ページネーション管理のインスタンスを返す
+     */
+    public static PaginationManager paginate(HttpServletRequest request) throws SQLException {
+        final int totalCount = count(request);
+        int currentPage = 1;
+        try {
+            currentPage = Integer.parseInt(request.getParameter("p"));
+        } catch (NumberFormatException ignored) {
+        }
+
+        return new PaginationManager(
+            totalCount,
+            ITEMS_PER_PAGE,
+            currentPage,
+            DISPLAY_LINKS,
+            LEFT_MARGIN
+        );
+    }
+
+    /**
+     * @return 商品名の部分一致による商品検索結果が、全部で何件か
+     */
+    public static int count(HttpServletRequest request) throws SQLException {
+        String keyword = request.getParameter("keyword");
+
+        if (null == keyword) {
+            keyword = "";
+        }
+
+        return Item.count(keyword);
     }
 }
